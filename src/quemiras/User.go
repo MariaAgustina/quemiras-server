3fe9fad6896d9453d91ebca2e89c33b542175055
agustinanobly <agustina@noblypos.com>
2020-07-18 22:02:55 -0300

    the users are now saved in mongo db

--- src/quemiras/User.go
+++ src/quemiras/User.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+    "context"
     "encoding/json"
-    "net/http"
     "fmt"
+    "net/http"
+    "time"
     "github.com/gorilla/mux"
+    "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 //para que lo desencodee bien tengo que declarar las variables con mayuscula
 type User struct {
-    ID      string  `json:"id,omitempty"`
-    Name    string  `json:"name"`
+    ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+    Name string             `json:"name,omitempty" bson:"name,omitempty"`
 }
 
 //TODO: in other file
@@ -20,38 +24,66 @@ type ProfileConfigLikes struct {
 
 var usersArray []User
 
-func GetUserEndpoint(w http.ResponseWriter, req *http.Request) {
-    params := mux.Vars(req)
-    for _, item := range usersArray {
-        if item.ID == params["id"] {
-            json.NewEncoder(w).Encode(item)
-            return
-        }
-    }
-    json.NewEncoder(w).Encode(&User{})
-}
-
-func CreateUserEndpoint(w http.ResponseWriter, req *http.Request) {
-    params := mux.Vars(req)
+func GetUserEndpoint(response http.ResponseWriter, request *http.Request) {
+    response.Header().Set("content-type", "application/json")
+    params := mux.Vars(request)
+    id, _ := primitive.ObjectIDFromHex(params["id"])
     var user User
-    _ = json.NewDecoder(req.Body).Decode(&user)
-    fmt.Printf("New user is %s", user.Name)
-    user.ID = params["id"]
-    usersArray = append(usersArray, user)
-    json.NewEncoder(w).Encode(user)
-}
-
-func DeleteUserEndpoint(w http.ResponseWriter, req *http.Request) {
-    params := mux.Vars(req)
-    for index, item := range usersArray {
-        if item.ID == params["id"] {
-            usersArray = append(usersArray[:index], usersArray[index+1:]...)
-            break
-        }
+    collection := client.Database("quemiras").Collection("users")
+    ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+    err := collection.FindOne(ctx, User{ID: id}).Decode(&user)
+    if err != nil {
+        response.WriteHeader(http.StatusInternalServerError)
+        response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+        return
     }
-    json.NewEncoder(w).Encode(usersArray)
+    json.NewEncoder(response).Encode(user)
 }
 
-func GetUsersArrayEndpoint(w http.ResponseWriter, req *http.Request) {
-    json.NewEncoder(w).Encode(usersArray)
+func CreateUserEndpoint(response http.ResponseWriter, request *http.Request) {
+    response.Header().Set("content-type", "application/json")
+    var user User
+    _ = json.NewDecoder(request.Body).Decode(&user)
+    collection := client.Database("quemiras").Collection("users")
+    ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+    result, _ := collection.InsertOne(ctx, user)
+    fmt.Printf("New user inserted %s \n", user.Name)
+    json.NewEncoder(response).Encode(result)
+}
+
+//TODO
+// func DeleteUserEndpoint(w http.ResponseWriter, req *http.Request) {
+//     params := mux.Vars(req)
+//     for index, item := range usersArray {
+//         if item.ID == params["id"] {
+//             usersArray = append(usersArray[:index], usersArray[index+1:]...)
+//             break
+//         }
+//     }
+//     json.NewEncoder(w).Encode(usersArray)
+// }
+
+func GetUsersArrayEndpoint(response http.ResponseWriter, request *http.Request) {
+    response.Header().Set("content-type", "application/json")
+    var usersArray []User
+    collection := client.Database("quemiras").Collection("users")
+    ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+    cursor, err := collection.Find(ctx, bson.M{})
+    if err != nil {
+        response.WriteHeader(http.StatusInternalServerError)
+        response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+        return
+    }
+    defer cursor.Close(ctx)
+    for cursor.Next(ctx) {
+        var user User
+        cursor.Decode(&user)
+        usersArray = append(usersArray, user)
+    }
+    if err := cursor.Err(); err != nil {
+        response.WriteHeader(http.StatusInternalServerError)
+        response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+        return
+    }
+    json.NewEncoder(response).Encode(usersArray)
 }
--- src/quemiras/main.go
+++ src/quemiras/main.go
@@ -1,74 +1,33 @@
 package main
 
 import (
-    "encoding/json"
+    "context"
+    "fmt"
     "log"
     "net/http"
+    "time"
     "github.com/gorilla/mux"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Person struct {
-    ID        string   `json:"id,omitempty"`
-    Firstname string   `json:"firstname,omitempty"`
-    Lastname  string   `json:"lastname,omitempty"`
-    Address   *Address `json:"address,omitempty"`
-}
 
-type Address struct {
-    City  string `json:"city,omitempty"`
-    State string `json:"state,omitempty"`
-}
-
-var people []Person
-
-func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
-    params := mux.Vars(req)
-    for _, item := range people {
-        if item.ID == params["id"] {
-            json.NewEncoder(w).Encode(item)
-            return
-        }
-    }
-    json.NewEncoder(w).Encode(&Person{})
-}
-
-func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
-    params := mux.Vars(req)
-    var person Person
-    _ = json.NewDecoder(req.Body).Decode(&person)
-    person.ID = params["id"]
-    people = append(people, person)
-    json.NewEncoder(w).Encode(people)
-}
-
-func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
-    params := mux.Vars(req)
-    for index, item := range people {
-        if item.ID == params["id"] {
-            people = append(people[:index], people[index+1:]...)
-            break
-        }
-    }
-    json.NewEncoder(w).Encode(people)
-}
-
-func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
-    json.NewEncoder(w).Encode(people)
-}
+var client *mongo.Client
 
 func main() {
+
+    fmt.Println("Starting the application...")
+    ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+    clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+    client, _ = mongo.Connect(ctx, clientOptions)
+
     router := mux.NewRouter()
-    people = append(people, Person{ID: "1", Firstname: "Nic", Lastname: "Raboy", Address: &Address{City: "Dublin", State: "CA"}})
-    people = append(people, Person{ID: "2", Firstname: "Maria", Lastname: "Raboy"})
-    router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
-    router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
-    router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
-    router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
 
     router.HandleFunc("/users", GetUsersArrayEndpoint).Methods("GET")
     router.HandleFunc("/user/{id}", GetUserEndpoint).Methods("GET")
     router.HandleFunc("/user/{id}", CreateUserEndpoint).Methods("POST")
-    router.HandleFunc("/user/{id}", DeleteUserEndpoint).Methods("DELETE")
+    //TODO
+    // router.HandleFunc("/user/{id}", DeleteUserEndpoint).Methods("DELETE")
 
     log.Fatal(http.ListenAndServe(":12345", router))
 }
